7292cdc3bc53cf530719053b0fe7bc4b38ee3f0e
djedjethai <djedje.thai.ok@gmail.com>
2020-05-28 21:29:18 +0700

    tuy

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-    return <div>I am commentBox</div>
+
+class CommentBox extends Component {
+    state = { comment: ''};
+
+    commentHandler = (event) => {
+        this.setState({ comment: event.target.value })
+    };
+
+    formSubmitHandler = (event) => {
+        event.preventDefault();
+
+        // TODO - call an action creator 
+        // and save the comment
+        
+        this.setState({ comment: '' });
+    }
+
+    render() {
+
+        return (
+            <form 
+                onSubmit={this.formSubmitHandler}
+            >
+                <h4>Add a comment </h4>
+                <textarea 
+                    value={this.state.comment}
+                    onChange={this.commentHandler}
+                />
+                <div>
+                    <button type="submit">Submit comment</button>
+                </div>
+            </form>
+        );
+    };
+
 }
+export default CommentBox;
--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -3,7 +3,24 @@ import { shallow } from 'enzyme';
 
 import CommentBox from 'components/CommentBox';
 
+// here we use Full DOM ่just for show
+// NORMALY WE SOULD USE SHALLOW as we test only this component
+
 test('show a div', () => {
     const wrapped = shallow(<CommentBox />);
     expect(wrapped.find('div').length).toBe(1);
-})
+});
+
+test('show a textArea and a button', () => {
+    const wrapper = shallow(<CommentBox />);
+    expect(wrapper.find('textarea').length).toBe(1);
+    expect(wrapper.find('button').length).toBe(1);
+});
+
+test('user can input text and submit', () => {
+
+});
+
+test('when submit textArea get empty', () => {
+
+});
