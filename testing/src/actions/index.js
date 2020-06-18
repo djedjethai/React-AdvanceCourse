69bd7b8845492a25d2b38b633568b26539f86c54
djedjethai <djedje.thai.ok@gmail.com>
2020-06-18 18:48:48 +0700

    start test async req2

--- testing/src/actions/index.js
+++ testing/src/actions/index.js
@@ -1,8 +1,19 @@
-import { ADD_COMMENT } from 'actions/types';
+import axios from 'axios';
+
+import { ADD_COMMENT, FETCH_COMMENTS } from 'actions/types';
+
 
 export function addComment(comment) {
     return {
         type: ADD_COMMENT,
         payload: comment
+    }
+}
+
+export function fetchComments() {
+    const comments = axios.get('http://jsonplaceholder.typicode.com/comments');
+    return {
+        type: FETCH_COMMENTS,
+        payload: comments
     }
 }
