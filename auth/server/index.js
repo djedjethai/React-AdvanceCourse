8b4b948a7286d9142f7eaea395ff1d1299e7a554
djedjethai <djedje.thai.ok@gmail.com>
2020-07-03 13:58:14 +0700

    server node done

---
+++ auth/server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+const router = require('./router');
+
+require('dotenv').config();
+const MONGODB_URI = process.env.MONGODB_URI;
+
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+
+mongoose.connect(MONGODB_URI)
+	.then(result => {
+		const port = process.env.PORT || 3090;
+		const server = http.createServer(app);
+		server.listen(port);
+		console.log('Server listen on ', port);
+	})
+	.catch(e => console.log(e));
+
