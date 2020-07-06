615eb87a2d1df045c3f32ffd7497f998a13399c8
djedjethai <djedje.thai.ok@gmail.com>
2020-07-06 17:10:50 +0700

    bcrypt added

--- auth/server/controllers/authentication.js
+++ auth/server/controllers/authentication.js
@@ -1,8 +1,21 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+
+
+require('dotenv').config();
+const SECRET = process.env.SECRET;
+
+function tokenForUser(user) {
+	const timestamp = new Date().getTime();
+	return jwt.encode({sub: user.id, iat: timestamp}, SECRET)
+}
 
 exports.signup = function(req, res, next) {
 	User.findOne({email: req.body.email})
 		.then(response => {
+			if (!req.body.email || !req.body.password) {
+				return res.status(422).send({error: 'you must provide email and password'});
+			}
 			if (response) {
 				return res.status(422).send({error: 'email is in use'});
 			}
@@ -10,16 +23,16 @@ exports.signup = function(req, res, next) {
 				email: req.body.email,
 				password: req.body.password
 			})
-			return newUser.save();
-
-		})
-		.then(respDb => {
-			console.log(respDb);
-			res.json({
-				id: respDb._id,
-				email: respDb.email,
-				password: respDb.password
-			});
+			newUser.save()
+				.then(respDb => {
+					console.log(respDb);
+					res.json({
+						id: respDb._id,
+						email: respDb.email,
+						token: tokenForUser(newUser)
+					})
+				})
+				.catch(e => next(e));
 		})
 		.catch(e => next(e));
 }
--- auth/server/models/user.js
+++ auth/server/models/user.js
@@ -1,10 +1,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 const userSchema = new Schema({
 	email: { type: String, unique: true, lowercase: true },
 	password: String
 });
+
+// on save hook, encrypt password
+// .pre() is an action wich happend before, so i should be able to use it with bcryptjs package....
+userSchema.pre('save', function(next) {
+	// user become an instance of user model (userSchema)
+	const user = this;
+
+	// generate a salt then run callback
+	bcrypt.genSalt(10, function(err, salt) {
+		if (err) {
+			return next(err);
+		}
+
+		// hash(encrypt) our password using the salt 
+		bcrypt.hash(user.password, salt, null, function(err, hash) {
+			if (err) {
+				return next(err);
+			}
+
+			user.password = hash;
+			next();
+		})
+
+	})
+})
 
 const ModelClass = mongoose.model('user', userSchema);
 
