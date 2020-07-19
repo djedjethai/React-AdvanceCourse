0c255a9eca34263f62ca10732c5f27a72b8ff827
djedjethai <djedje.thai.ok@gmail.com>
2020-07-19 14:37:48 +0700

    course done

--- auth/my-app/src/components/Header.js
+++ auth/my-app/src/components/Header.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import './HeaderStyle.css';
 
 class Header extends Component {
-	render() {
-		return(
-			<div>
-				<Link to="/">Redux Auth</Link>
+	renderList() {
+		if (!this.props.auth) {
+			return (<div>
 				<Link to="/signup">Sign up</Link>
 				<Link to="/signin">Sign in</Link>
+			</div>);
+		} else {
+			return (<div>
 				<Link to="/signout">Sign out</Link>
 				<Link to="/feature">Feature</Link>
+			</div>);
+		}		
+	};
+
+	render() {
+		return(
+			<div className="header">
+				<Link to="/">Redux Auth</Link>
+				{this.renderList()}				
 			</div>
 		);
 	}
 }
 
-export default Header;
+const mapStateToProps = (state) => {
+	return {
+		auth: state.auth.authenticated	
+	}
+};
+
+
+export default connect(mapStateToProps, null)(Header);
